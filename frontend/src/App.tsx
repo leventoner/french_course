088d643a5de45c3903744270b6ffd03ec4bc905f
leventoner <lener1903@hotmail.com>
2026-02-10 22:58:36 +0100

    fix: import useEffect in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import { useAuthStore } from './stores/authStore';
